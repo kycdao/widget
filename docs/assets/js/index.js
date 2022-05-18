59db2fe462525e5ebcc3428c62f01f13cf040b27
Gabor Horvath <gabor.horvath@runiosit.com>
2022-05-19 00:39:13 +0200

    added bootstrap-select plugin and country options list loading, fixed step descriptions and submit buttons

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -21,32 +21,36 @@ const getKycDaoApiStatus = async () => {
 
   const elem = document.getElementById("api-status");
   elem.innerHTML = status.apiStatus;
-}
+};
 
 const setupLoginDance = (actualLoginFn) => {
-    document.getElementById(
-      "near-status"
-    ).innerHTML = `connected to ${kycDao.nearConfig.nodeUrl}`;
+  document.getElementById(
+    "near-status"
+  ).innerHTML = `connected to ${kycDao.nearConfig.nodeUrl}`;
 
-    if (
-      !kycDao.chainAndAddress ||
-      kycDao.chainAndAddress.blockchain !== "Near"
-    ) {
-      document
-        .getElementById("near-login")
-        .addEventListener(
-          "click",
-          actualLoginFn
-        );
-    } else {
-      const b = document.getElementById("near-login");
-      b.innerHTML = `hi ${kycDao.chainAndAddress.address}`;
-      b.setAttribute("disabled", "yes");
-    }
-}
+  if (!kycDao.chainAndAddress || kycDao.chainAndAddress.blockchain !== "Near") {
+    document
+      .getElementById("near-login")
+      .addEventListener("click", actualLoginFn);
+  } else {
+    const b = document.getElementById("near-login");
+    b.innerHTML = `hi ${kycDao.chainAndAddress.address}`;
+    b.setAttribute("disabled", "yes");
+  }
+};
 
 const main = () => {
   (async () => {
+    // add tax residency picker country options
+    const taxResidencyPicker = document.getElementById("tax-residency");
+
+    for (const country of kycDaoSdk.COUNTRIES) {
+      const option = document.createElement("option");
+      option.text = country.name;
+      option.value = country.iso_cca2;
+      taxResidencyPicker.add(option);
+    }
+
     // 1. Check initialized API status
     await getKycDaoApiStatus();
     document
@@ -54,9 +58,9 @@ const main = () => {
       .addEventListener("click", getKycDaoApiStatus);
 
     // 2. web3 login (NEAR network)
-    const web3login = () => kycDao.connectWallet("Near")
-    setupLoginDance(web3login)
-    
+    const web3login = () => kycDao.connectWallet("Near");
+    setupLoginDance(web3login);
+
     // 3. Check if wallet has kycDAO NFT (on-chain)
     // ⚠️ only use this for access control on your backend (server-side) ⚠️
     document
@@ -73,13 +77,12 @@ const main = () => {
         // TODO handle backend polling + add indicator
       });
 
-    
     // 5. mint kycNFT
     document
       .getElementById("start-verification")
       .addEventListener("click", async () => {
         // TODO TODO send in data with kycDao.startMinting, handle polling, add indicator
-      });    
+      });
   })();
 };
 
