f704537d2b73a37af8d20afb39b21e59a7067d75
Pas <pas@zomg.hu>
2022-11-02 14:53:24 +0100

    remove debug log from config

--- config-overrides.js
+++ config-overrides.js
@@ -8,23 +8,19 @@ module.exports = function override(config, env) {
         ? { ...config.resolve.fallback, crypto: require.resolve("crypto-browserify") }
         : { crypto: require.resolve("crypto-browserify") }
 
-    console.log('xxx', process.env.npm_package_main)
 
     const outFile = path.basename('dist/index.js');
     
-    console.log('xxx000')
 
     const outDir = "./build"
     config.target = 'web'
     config.entry = ["./src/index.tsx", "./src/KycDaoClient.ts"]
     config.output.filename = outFile
 
-    console.log('abc1')
 
     config.output.path = path.resolve(outDir)
     config.resolve.fallback = fallback
 
-    console.log('abc')
 
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
@@ -36,7 +32,6 @@ module.exports = function override(config, env) {
     ])
 
 
-    console.log('yyy')
 
     if (env === 'production') {
         config.plugins.push(new NpmDtsPlugin())
