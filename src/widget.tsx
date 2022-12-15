db1fbf5b6f93811212ea36b9a7fe0673434ededd
AttilaVargaw <attilavargaw@gmail.com>
2022-12-15 18:53:06 +0100

    Fix widget size

--- src/widget.tsx
+++ src/widget.tsx
@@ -3,11 +3,18 @@ import { FC, useEffect, useRef } from "react"
 import "./style/index.scss"
 import { KycDaoClient } from "./KycDaoClient"
 import {
+	IframeOptions,
 	KycDaoClientInterface,
 	KycDaoClientOptions,
 } from "./KycDaoClientCommon"
+import { SdkConfiguration } from "@kycdao/kycdao-sdk"
 
-export type KycDaoWidgetConfig = KycDaoClientOptions
+export type KycDaoWidgetConfig = {
+	parent: HTMLElement | string
+	config: SdkConfiguration
+	onFail?: (reason: string) => void
+	onSuccess?: (data?: string) => void
+}
 
 export const KycDaoWidget: FC<KycDaoWidgetConfig> = (config) => {
 	const parent = useRef<HTMLDivElement>(null)
@@ -19,7 +26,12 @@ export const KycDaoWidget: FC<KycDaoWidgetConfig> = (config) => {
 		}
 
 		if (parent.current) {
-			client.current = new ClientConstructor(config)
+			client.current = new ClientConstructor({
+				...config,
+				width: 400,
+				height: 650,
+				configFromUrl: false,
+			})
 			client.current.open()
 		} else {
 			console.error("Something went wrong, check the widget creator function!")
