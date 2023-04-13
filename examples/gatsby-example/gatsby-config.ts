0df6f643a9fd5c7119d771f06709c9c78708538a
Balázs <balazs@otakomaiya.com>
2023-04-13 13:35:45 +0200

    Gatsby example

---
+++ examples/gatsby-example/gatsby-config.ts
@@ -0,0 +1,16 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+	siteMetadata: {
+		title: `web-sdk-gatsby-test`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [`gatsby-plugin-pnpm`],
+	pathPrefix: '/integration-examples/evm-gatsby-example',
+}
+
+export default config
---
+++ examples/gatsby-example/src/pages/iframe.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react"
+import { KycDaoClientInterface } from "@kycdao/widget/dist/KycDaoClientCommon"
+
+const IframePage: React.FC = () => {
+	const iframeClient = useRef<KycDaoClientInterface>()
+
+	useEffect(() => {
+		import("@kycdao/widget").then((module) => {
+			iframeClient.current = new module.KycDaoIframeClient({
+				parent: "#modalroot",
+				iframeOptions: {
+					url: "https://sdk.kycdao.xyz/iframe.html",
+					messageTargetOrigin: window.origin,
+				},
+				config: {
+					demoMode: true,
+					enabledBlockchainNetworks: ["PolygonMumbai"],
+					enabledVerificationTypes: ["KYC"],
+					evmProvider: "ethereum",
+					baseUrl: "https://staging.kycdao.xyz",
+				},
+			})
+		})
+	}, [])
+
+	return (
+		<div>
+			<h1>Iframe integration</h1>
+
+			<button onClick={() => iframeClient.current?.open()}>Open modal</button>
+
+			<div id="modalroot" />
+		</div>
+	)
+}
+
+export default IframePage
+
+export const Head = () => <title>Iframe example</title>
